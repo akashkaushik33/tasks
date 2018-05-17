16d8aa0c825e58cac2e4596bbea5bfb18f48204f
Cryptocracy <admin@cryptocracy.io>
2018-05-16 19:29:40 -0700

    adding task modal
    adding task modal

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -33,9 +33,7 @@
               <label>
                 {{ project.text }}
               </label>
-              <button @click.prevent=""
-                class="btn btn-default"
-                href="#">Add Task</button>
+              <button id="show-modal" @click="showModal = true" class="btn btn-default" href="#">Add Task</button>
               <a @click.prevent="projects.splice(projects.indexOf(project), 1)"
                 class="delete pull-right"
                 href="#">Remove Project</a>
@@ -143,3 +141,35 @@ label {
   }
 }
 </style>
+
+<script type="text/x-template" id="modal-template">
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</script>
